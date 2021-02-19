35ad61938c23f4f598432150aa4aa45140570e8d
Hein Wessels <heinwessels93@gmail.com>
2021-02-19 22:51:38 +0100

    Already first bugfix.

--- modfiles/control.lua
+++ modfiles/control.lua
@@ -1,10 +1,9 @@
 small_pole = require("scripts.fluidic-small-pole")
-medium_pole_out = require("scripts.fluidic-medium-pole")
+medium_pole = require("scripts.fluidic-medium-pole")
 
 function creation_event (event)
     small_pole.on_entity_created(event)
-    medium_pole_out.on_entity_created(event)
-    medium_pole_in.on_entity_created(event)
+    medium_pole.on_entity_created(event)
 end
 
 script.on_event(defines.events.on_built_entity, creation_event)
@@ -14,8 +13,7 @@ script.on_event(defines.events.script_raised_revive, creation_event)
 
 function removal_event (event)
     small_pole.on_entity_removed(event)
-    medium_pole_out.on_entity_removed(event)
-    medium_pole_in.on_entity_removed(event)
+    medium_pole.on_entity_removed(event)    
 end
 
 script.on_event(defines.events.on_player_mined_entity, removal_event)
