968a708f7218323620e286a5a71d4cc80b8077bf
Hein Wessels <heinwessels93@gmail.com>
2021-02-23 22:08:22 +0100

    Optimized overlay algorithm significantly with appropriate depth setting.

--- modfiles/scripts/fluidic-build-tools.lua
+++ modfiles/scripts/fluidic-build-tools.lua
@@ -1,22 +1,32 @@
-
+local script_data =
+{
+    current_overlay_target = nil
+}
 
 script.on_event(defines.events.on_tick, function (event)
 
     local player = game.players[1]
 
-    if player.selected then        
-        local entity = player.selected
-        if string.match(entity.name, "fluidic") then
-            -- The player currently has his mouse over a fluidic entity
+    if player.selected and string.match(player.selected.name, "fluidic") then        
+        local entity = player.selected        
+        -- The player currently has his mouse over a fluidic entity
+        
+        if entity ~= script_data.current_overlay_target then
+            -- It's a new target!            
+            reset_rendering()   -- Make sure we don't have any left-over overlays
             
-            -- TODO Make depth a setting
-            show_fluidic_entity_connections(entity, 5)
+            script_data.current_overlay_target = entity
+            show_fluidic_entity_connections(entity, settings.global["fluidic-electric-overlay-depth"].value)
+        
         else
-            -- TODO Only reset if there is something to reset
-            reset_rendering()
+            -- We've already drawn the overlay for this target. Do nothing but wait.
         end
     else
-        reset_rendering()
+        -- Not looking at anything important
+        if script_data.current_overlay_target ~= nil then            
+            reset_rendering()
+            script_data.current_overlay_target = nil
+        end
     end 
 end)
 
@@ -25,6 +35,7 @@ function show_fluidic_entity_connections(
     entity, depth, black_list
 )   
     if depth == 0 then return end   -- Recursion stop
+    if black_list == nil then black_list = {} end
     for _, neighbours_section in ipairs(entity.neighbours) do
         if next(neighbours_section) == nil then break end    -- Table is empty
         for _, neighbour in ipairs(neighbours_section) do
@@ -38,31 +49,35 @@ function show_fluidic_entity_connections(
             end
 
             -- Are we coming from this neighbour?
-            -- TODO Verify that this works
             if black_list ~= nil then
-                if neighbour == black_list then should_draw_connection = false end
-            end
-            
-            -- TODO multiple fluidboxes could go to same entity
+                -- if neighbour == black_list then should_draw_connection = false end
+                if black_list[neighbour.unit_number] ~= nil then
+                    -- TODO This elimination is too harsh, get better solution
+                    -- Currently it doesn't draw nice diamonds.
+                    should_draw_connection = false
+                end
+            end            
 
             if should_draw_connection then
                 -- This is a new neighbour
-                draw_connection(entity, neighbour)
+                -- TODO draw from the fluidbox, not the entity
+                draw_connection(entity, entity.position, neighbour.position)
 
                 -- Recursively draw neighbours' neighbours after blacklisting the current entity
-                show_fluidic_entity_connections(neighbour, depth - 1, entity)
+                black_list[entity.unit_number] = depth
+                show_fluidic_entity_connections(neighbour, depth - 1, black_list)
             end 
         end 
     end
 end
 
-function draw_connection(this, that)
+function draw_connection(entity, here, there)
     rendering.draw_line{
-        color = {r = 0,  g = 0.2, b = 0, a = 0},
+        color = {r = 0,  g = 0.6, b = 0, a = 0},
         width = 3,
-        from = this.position,
-        to = that.position,
-        surface = this.surface,
+        from = here,
+        to = there,
+        surface = entity.surface,
         players = players,
     }
     for _, point in ipairs{this, that} do
@@ -70,8 +85,8 @@ function draw_connection(this, that)
             color = {r = 0,  g = 0.5, b = 0, a = 0},
             radius = 0.25,
             width = 3,
-            target = point.position,
-            surface = this.surface,
+            target = point,
+            surface = entity.surface,
             players = players,
         }
     end
---
+++ modfiles/settings.lua
@@ -0,0 +1,10 @@
+data:extend({
+    {    
+        type = "int-setting",
+        name = "fluidic-electric-overlay-depth",           
+        setting_type = "runtime-global",        
+        default_value = 5,
+        minimum_value = 0,
+        maximum_value = 20
+    },
+})
