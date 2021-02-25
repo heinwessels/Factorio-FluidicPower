4787cd1e3048cc2e67d4344e521af3b658b166da
Hein Wessels <heinwessels93@gmail.com>
2021-02-25 17:10:28 +0100

    Just an update.

--- modfiles/prototypes/fluidic-medium-pole.lua
+++ modfiles/prototypes/fluidic-medium-pole.lua
@@ -1,26 +1,22 @@
-data:extend
-({   
-    {
-        type = "item",
-        name = "fluidic-medium-pole-in",        
-		icon_size = 64,
-	    place_result = "fluidic-medium-pole-in",
-        icon = "__base__/graphics/icons/medium-electric-pole.png",
-		subgroup = "energy-pipe-distribution",
-		order = "b[pipe]-b[duct]-a[duct-small]a",
-        stack_size = 50
-    },
-    {
-        type = "item",
-        name = "fluidic-medium-pole-out",
-		icon_size = 64,
-	    place_result = "fluidic-medium-pole-out",
-        icon = "__base__/graphics/icons/medium-electric-pole.png",
-		subgroup = "energy-pipe-distribution",
-		order = "b[pipe]-b[duct]-a[duct-small]a",
-        stack_size = 50
-    }
-})
+local item = table.deepcopy(data.raw["item"]["medium-electric-pole"])
+override = {
+    name = "fluidic-medium-pole-in",
+    place_result = "fluidic-medium-pole-in",
+}
+for k,v in pairs(override) do
+    item[k]=v
+end
+data:extend({item})
+
+local item = table.deepcopy(data.raw["item"]["medium-electric-pole"])
+override = {
+    name = "fluidic-medium-pole-out",
+    place_result = "fluidic-medium-pole-out",
+}
+for k,v in pairs(override) do
+    item[k]=v
+end
+data:extend({item})
 
 local pole = table.deepcopy(data.raw["electric-pole"]["medium-electric-pole"])
 override = {
@@ -39,9 +35,7 @@ override = {
         usage_priority = "secondary-input",
         drain = "0kW"  
     },
-    collision_box = {{-0.15, -0.15}, {0.15, 0.15}},
-    selection_box = {{-0.5, -0.5}, {0.5, 0.5}},
-    drawing_box = {{-0.5, -2.8}, {0.5, 0.5}},
+    selection_box = {{0,0},{0,0}}, -- <fluidic-medium-pole> is what will be selectable
     maximum_wire_distance = 0,
     open_sound = nil,
     close_sound = nil,
@@ -100,19 +94,31 @@ override = {
         type = "electric",
         usage_priority = "tertiary"
     },
+
+    -- TODO Disable these graphics
     vertical_animation = pole.pictures,
-    horizontal_animation = pole.pictures,
+    horizontal_animation = pole.pictures,    
 }
 for k,v in pairs(override) do
     pole[k]=v
 end
 data:extend({pole})
 
+local dummy = table.deepcopy(data.raw["electric-pole"]["medium-electric-pole"])
+override = {    
+    name = "fluidic-medium-pole-out-electric",
+    minable = {mining_time = 0.1, result = "fluidic-medium-pole-out"},   
+    maximum_wire_distance = 1,    
+}
+for k,v in pairs(override) do
+    dummy[k]=v
+end
+data:extend({dummy})
 
 local dummy = table.deepcopy(data.raw["electric-pole"]["medium-electric-pole"])
 override = {    
-    name = "fluidic-medium-pole-dummy",    
-    selection_box = {{0,0},{0,0}},    
+    name = "fluidic-medium-pole-in-electric",
+    minable = {mining_time = 0.1, result = "fluidic-medium-pole-in"},   
     maximum_wire_distance = 1,    
 }
 for k,v in pairs(override) do
--- modfiles/scripts/fluidic-medium-pole.lua
+++ modfiles/scripts/fluidic-medium-pole.lua
@@ -9,22 +9,34 @@ function power_pole.on_entity_created(event)
         entity = event.created_entity
     end
     if not entity then return end
-    if entity.name == "fluidic-medium-pole-in" or entity.name == "fluidic-medium-pole-out" then        
+    if entity.name == "fluidic-medium-pole-in" then        
         local pole = entity.surface.create_entity{
-            name = "fluidic-medium-pole-dummy",
+            name = "fluidic-medium-pole-in-electric",
             position = {x = entity.position.x, y = entity.position.y},
             direction = entity.direction,                
-            force = entity.force
-        }        
+            force = entity.force,
+            raise_built = false
+        }
+    elseif entity.name == "fluidic-medium-pole-out" then
+        local pole = entity.surface.create_entity{
+            name = "fluidic-medium-pole-out-electric",
+            position = {x = entity.position.x, y = entity.position.y},
+            direction = entity.direction,                
+            force = entity.force,
+            raise_built = false
+        }
     end
 end
 
 function power_pole.on_entity_removed(event)
     if event.entity and event.entity.valid then
       local surface = event.entity.surface
-      if event.entity.name == "fluidic-medium-pole-in" or event.entity.name == "fluidic-medium-pole-out" then
-        local e = event.entity.surface.find_entity("fluidic-medium-pole-dummy", event.entity.position)
-        if e then e.destroy() end        
+      if event.entity.name == "fluidic-medium-pole-in" then
+        local e = event.entity.surface.find_entity("fluidic-medium-pole-in-electric", event.entity.position)
+        if e then e.destroy{raise_destroy=false} end        
+      elseif event.entity.name == "fluidic-medium-pole-out" then
+        local e = event.entity.surface.find_entity("fluidic-medium-pole-out-electric", event.entity.position)
+        if e then e.destroy{raise_destroy=false} end        
       end
     end
 end
