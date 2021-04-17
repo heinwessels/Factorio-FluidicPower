6fb6f5a74e6c9cd4954f3f04bb3e3a9eb890d376
Hein Wessels <heinwessels93@gmail.com>
2021-04-17 15:14:37 +0200

    Reworked accumulator a little.

--- modfiles/prototypes/fluidic-transformers.lua
+++ modfiles/prototypes/fluidic-transformers.lua
@@ -1,3 +1,6 @@
+local hit_effects = require ("__base__.prototypes.entity.hit-effects")
+local sounds = require("__base__.prototypes.entity.sounds")
+
 -- Create item
 data:extend({
     util.merge{
@@ -27,6 +30,7 @@ data:extend
             {"advanced-circuit", 5},
             {"copper-cable", 20}
         },
+        energy_required = 3,
         result = "fluidic-transformer"
     },
 
@@ -119,21 +123,32 @@ data:extend
 })
 
 -- Create the entities
-data:extend({util.merge{
-    -- This is a weird legacy hack where we use the accumulator shape, but create
-    -- a furnace with fluid inputs, using old beacon graphics. Don't judge me.
-    data.raw["electric-pole"]["substation"],
+data:extend({
     {
         type = "assembling-machine",
         name = "fluidic-transformer",
         icon = "__FluidicPower__/graphics/icons/transformer-icon.png",
         icon_size = 32,
-        mode = "output-to-separate-pipe",
+        flags = {"placeable-neutral", "placeable-player", "player-creation"},
+        minable = {mining_time = 0.2, result = "fluidic-transformer"},
+        max_health = 400,
+        corpse = "assembling-machine-1-remnants",
+        dying_explosion = "assembling-machine-1-explosion",
         crafting_categories = {"fluidic-transformers"},
-        minable = {result = "fluidic-transformer"},
+        mode = "output-to-separate-pipe",
         allowed_effects = {},
         module_specification = {module_slots=0},
-        crafting_speed = 1,        
+        crafting_speed = 1,
+        resistances =
+        {
+            {
+                type = "fire",
+                percent = 70
+            }
+        },
+        collision_box = {{-0.9, -0.9}, {0.9, 0.9}},
+        selection_box = {{-1, -1}, {1, 1}},
+        damaged_trigger_effect = hit_effects.entity(),
         energy_usage = "1W",    -- Not used
         energy_source = {
             type = "void",        
@@ -163,7 +178,7 @@ data:extend({util.merge{
                     {type = "output", position = {1.5, -0.5}, max_underground_distance = 3},
                 },        
             },
-        },        
+        }, 
         animation =
         {
             layers =
@@ -198,7 +213,27 @@ data:extend({util.merge{
                     animation_speed = 2,
                     scale = 0.8
                 },                
-            }
+            },
+        },   
+        open_sound = sounds.machine_open,
+        close_sound = sounds.machine_close,
+        vehicle_impact_sound = sounds.generic_impact,
+        working_sound =
+        {
+            sound =
+            {
+              filename = "__base__/sound/accumulator-working.ogg",
+              volume = 0.5
+            },
+            idle_sound =
+            {
+              filename = "__base__/sound/accumulator-idle.ogg",
+              volume = 0.35
+            },
+            max_sounds_per_type = 3,
+            audible_distance_modifier = 0.6,
+            fade_in_ticks = 2,
+            fade_out_ticks = 20
         },
-    }
-}})
+    },
+})
