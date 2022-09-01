be75a0d149bb9e88d38ccf4e21e513d3997f210a
heinwessels <heinwessels93@gmail.com>
2022-09-01 17:22:02 +0200

    Improved accumulator gen and added K2

--- prototypes/fluidic-accumulator-generator.lua
+++ prototypes/fluidic-accumulator-generator.lua
@@ -1,5 +1,5 @@
 
--- Recipe will be the vanilla accumulator recipe
+util = require("util")
 
 -- Create the entity
 local circuit_connections = circuit_connector_definitions.create(
@@ -20,59 +20,81 @@ local Generator = { }
 function Generator.create_accumulator(context)
 
     local name = "fluidic-"..context.base_name
+    local base_entity = data.raw.accumulator[context.base_name]
+    context.fluid_filter = context.fluid_filter or "fluidic-10-kilojoules"
 
     -- Item will be the base item (for compatability)
     data.raw.item[context.base_name].place_result = name
 
+    -- We assume the accumulator is square, and centered on the center :P
+    fluidbox_offset = math.ceil(base_entity.collision_box[2][1]) + 0.5
+    fluid_base_divider = 100 * util.parse_energy(data.raw.fluid[context.fluid_filter].fuel_value)
+
     -- Create entity
     local tank = data.raw["storage-tank"]["storage-tank"]
-    data:extend({
-        util.merge{
-            data.raw.accumulator[context.base_name],
-            {
-                name = name,
-                type = "storage-tank",
-                minable = {mining_time = 0.5, result = context.base_name},
-                flow_length_in_ticks = 360,
-                fluid_box =
-                {
-                    -- Energy Storage = base_area x fluid_unit x 100 = 5000 kJ
-                    -- So basically it's the contents of 5 poles in a 2x2 area.
-                    -- Not very efficient, but how vanilla works.
-                    base_area = 5,
-                    filter = "fluidic-10-kilojoules",      
-                    pipe_connections =
-                    {
-                        {type = "input-output", position = {-1.5, -0.5}, max_underground_distance = 1},
-                        {type = "input-output", position = {1.5, -0.5}, max_underground_distance = 1},
-                        {type = "input-output", position = { -0.5, -1.5,}, max_underground_distance = 1},
-                        {type = "input-output", position = { -0.5, 1.5}, max_underground_distance = 1},
-                    }
-                },
-                rotatable = false,
-                window_bounding_box = {{-0.2, 0.3}, {-0.4, 0.7}},
-                pictures = {
-                    picture = table.deepcopy(entity.picture),
-                    fluid_background = tank.pictures.fluid_background,
-                    window_background = tank.pictures.window_background,
-                    flow_sprite = tank.pictures.flow_sprite,
-                    gas_flow = tank.pictures.gas_flow,
-                },    
-                circuit_wire_connection_points = circuit_connections.points,
-                circuit_connector_sprites = circuit_connections.sprites,
-                circuit_wire_max_distance = default_circuit_wire_max_distance,            
+    local accumulator = {
+        type = "storage-tank",
+        name = name,
+        localised_name = {"entity-name."..context.base_name},
+        localised_description={"", 
+            {"fluidic-text.accumulator"}, 
+            {"fluidic-text.accumulator-rating", base_entity.energy_source.buffer_capacity}
+        },
+        icon = base_entity.icon,
+        icon_size = base_entity.icon_size, 
+        icon_mipmaps = base_entity.icon_mipmaps,
+        flags = base_entity.flags,
+        minable = {mining_time = base_entity.minable.mining_time, result = context.base_name},
+        max_health = base_entity.max_health,
+        corpse = base_entity.corpse,
+        dying_explosion = base_entity.dying_explosion,
+        collision_box = base_entity.collision_box,
+        selection_box = base_entity.selection_box,
+        damaged_trigger_effect = base_entity.damaged_trigger_effect,
+        fluid_box = {
+            base_area = util.parse_energy(base_entity.energy_source.buffer_capacity) / fluid_base_divider,
+            filter = context.fluid_filter,
+            hide_connection_info = true,
+            pipe_connections =
+            {   
+                -- TODO custom
+                {type = "input-output", position = {-fluidbox_offset, -0.5}, max_underground_distance = 1},
+                {type = "input-output", position = {fluidbox_offset, -0.5}, max_underground_distance = 1},
+                {type = "input-output", position = { -0.5, -fluidbox_offset,}, max_underground_distance = 1},
+                {type = "input-output", position = { -0.5, fluidbox_offset}, max_underground_distance = 1},
             }
         },
-    })
-
-    accu = data.raw["storage-tank"][name]
-
-    table.insert(accu.flags, "not-rotatable")
+        rotatable = false,
+        window_bounding_box = {{0, 0}, {0, 0}},
+        pictures = {
+            picture = base_entity.picture,
+            fluid_background = tank.pictures.fluid_background,
+            window_background = tank.pictures.window_background,
+            flow_sprite = tank.pictures.flow_sprite,
+            gas_flow = tank.pictures.gas_flow,
+        },
+        flow_length_in_ticks = 360,
+        vehicle_impact_sound = base_entity.vehicle_impact_sound,
+        open_sound = base_entity.open_sound,
+        close_sound = base_entity.close_sound,
+        working_sound = base_entity.working_sound,  
+        circuit_wire_connection_points = circuit_connections.points,
+        circuit_connector_sprites = circuit_connections.sprites,
+        circuit_wire_max_distance = default_circuit_wire_max_distance,
+        water_reflection = base_entity.water_reflection
+    }
+      
+    table.insert(accumulator.flags, "not-rotatable")
     if settings.startup["fluidic-disable-accumulator-alt-info"].value then
         table.insert(accu.flags, "hide-alt-info")
     end
-
-    return accu
+    
+    data:extend({accumulator})
+    
+    -- Add "no-no" message to now-hidden entity
+    data.raw.accumulator[context.base_name].localised_description = {"fluidic-text.non-accessable"}
+    
+    return accumulator
 end
 
 return Generator
--- prototypes/fluidic-accumulators.lua
+++ prototypes/fluidic-accumulators.lua
@@ -1,9 +1,18 @@
 
 Generator = require("prototypes.fluidic-accumulator-generator")
 
-local accu = Generator.create_accumulator{
-    base_name = "accumulator"
-}
--- Vanilla item has special sprites
+-- Energy storage will remain the same, which means
+-- Energy Storage = base_area x fluid_unit x 100 = 5000 kJ
+-- So basically it's the contents of 5 poles in a 2x2 area.
+-- Not very efficient, but how vanilla works.
+
+-- Vanilla item has special sprites and a window
+local accu = Generator.create_accumulator{base_name = "accumulator"}
+accu.window_bounding_box = {{-0.2, 0.3}, {-0.4, 0.7}}
 accu.pictures.picture.layers[1].filename = "__FluidicPower__/graphics/entities/accumulator/accumulator.png"
 accu.pictures.picture.layers[1].hr_version.filename = "__FluidicPower__/graphics/entities/accumulator/hr-accumulator.png"
+
+
+if mods["Krastorio2"] then
+    local accu = Generator.create_accumulator{base_name = "kr-energy-storage"}
+end
