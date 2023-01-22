2a09bbcc3b1590caeb3a2003ac2364ba32d915fb
Hein Wessels <heinwessels93@gmail.com>
2023-01-22 21:04:37 +0100

    Fix regression where Transformer and Energy Sensor was no longer researchable

--- prototypes/fluidic-technology.lua
+++ prototypes/fluidic-technology.lua
@@ -5,6 +5,11 @@ local poles_to_check = {
     ["substation"] = true,
 }
 
+local add_recipe_when = {
+    ["big-electric-pole"] = "fluidic-transformer",
+    ["constant-combinator"] = "fluidic-energy-sensor"
+}
+
 if mods["Krastorio2"] and data.raw.recipe["kr-substation-mk2"] then
     poles_to_check["kr-substation-mk2"] = true
 end
@@ -14,6 +19,7 @@ for technology_name, technology in pairs(data.raw.technology) do
 
     local effects_to_add = { }
     for index, effect in pairs(technology.effects) do
+        -- Add added pole entities
         if poles_to_check[effect.recipe] then
             poles_to_check[effect.recipe] = false -- Remember to not `enable` recipe itself
             if data.raw.recipe["fluidic-"..effect.recipe.."-in"] then
@@ -24,13 +30,23 @@ for technology_name, technology in pairs(data.raw.technology) do
                 }
             end
         end
+
+        -- Add some other things
+        local recipe_to_add = add_recipe_when[effect.recipe]
+        if recipe_to_add then
+            effects_to_add["end"] = {
+                type = "unlock-recipe",
+                recipe = recipe_to_add
+            }
+        end
     end
 
     -- Now add the actual techs
     for index, effect in pairs(effects_to_add) do
         -- The index to place the in-pole next to the out-pole
         -- in the `This research unlocks` GUI
-        table.insert(technology.effects, index, effect)
+        local index_to_add = index == "end" and #technology.effects + 1 or index
+        table.insert(technology.effects, index_to_add, effect)
     end
 
     ::continue::
